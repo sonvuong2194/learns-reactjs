a5f53e994c4317a6fda9c72fad871e5bf0645c2b
Son Vuong <sonvuong2194@gmail.com>
2020-12-02 15:20:00 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 // import logo from "./logo.svg";
 // import "./App.css";
 
+import { Link, NavLink, Redirect, Route, Router, Switch } from "react-router-dom";
+import AlbumFeature from "./features/Album";
 import TodoFeature from "./features/Todo";
 
 function App() {
   return (
     <div className="App ">
-      <TodoFeature />
+      <h1>Home Page</h1>
+
+
+      <p><NavLink to="/todos">Todos</NavLink></p>
+      <p><NavLink to="/album" activeClassName="Son dang o day">Album</NavLink></p>
+      
+
+      <Switch>
+        <Redirect from="/home" to="/" exact/>
+        <Redirect from="/post-list/:postId" to="/post/:postId" exact/>
+
+      <Route path="/" component={TodoFeature} exact/>
+      <Route path="/todos" component={TodoFeature} />
+      <Route path="/album" component={AlbumFeature} />
+      </Switch>
     </div>
   );
 }
